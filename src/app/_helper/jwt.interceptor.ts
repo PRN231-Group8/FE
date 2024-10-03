57e57c7e3d0cf8f0646fceff1c33f83fe9b96cd0
kieutranquocson5 <kieutranquocson5@gmail.com>
2024-10-03 19:49:35 +0700

    feat: update UI login/regis

---
+++ src/app/_helper/jwt.interceptor.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../environments/environment';
+import { AuthenticationService } from '../services/authentication.service';
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+  constructor(private authenticationService: AuthenticationService) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<any>> {
+    // add auth header with jwt if user is logged in and request is to the api url
+    const user = this.authenticationService.userValue;
+    const token = user?.token;
+    const isLoggedIn = !!token;
+    const isApiUrl = request.url.startsWith(environment.BACKEND_API_URL);
+
+    if (isLoggedIn && isApiUrl) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    }
+
+    return next.handle(request);
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
-    production: false
-  };
+  production: false,
+  BACKEND_API_URL: 'https://localhost:7130',
+  FRONTEND_URL: 'http://localhost:4200',
+};
