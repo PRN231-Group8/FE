2adc0ad5d3c76e523aaadd538ee8dc2ac2981661
minhuy2411 <hoanganh241115@gmail.com>
2024-10-05 11:09:04 +0700

    feat: change api url backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://be-container-explorenow-duhthwhkecf0eef7.southeastasia-01.azurewebsites.net'
+    apiUrl: 'https://be-explore-now.azurewebsites.net/'
   };
